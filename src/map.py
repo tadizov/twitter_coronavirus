15c0a0c74c3ccd8a27dc39fca84928e253ca6d99
Your Name <you@example.com>
2020-05-03 21:39:11 -0700

    Pushing

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country= defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -50,7 +51,6 @@ with zipfile.ZipFile(args.input_path) as archive:
 
             # loop over each line in the inner file
             for line in f:
-
                 # load the tweet as a python dictionary
                 tweet = json.loads(line)
 
@@ -59,11 +59,21 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # search hashtags
                 for hashtag in hashtags:
-                    lang = tweet['lang']
+                    lang = tweet['lang'] #language
+
+                    #country
+                    try:
+                        country=tweet["place"]["country_code"]
+                    except TypeError:
+                        country_code='und'
+                        pass
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
-                    counter_lang['_all'][lang] += 1
-
+                        counter_country[hashtag][country] +=1
+                    
+                    counter_lang['_all']['lang']+=1  
+                    counter_country['_all']['country']+=1    
+                                            
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -72,7 +82,14 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
-print('saving',output_path_lang)
+output_path_country = output_path_base+'.country'
+
+print('saving_language',output_path_lang)
+print('saving_country',output_path_country)
+
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+
 
